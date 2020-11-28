db7031b3fb4e5b2b5bed0e248efdee1d6b4e9a26
Activx-nV <68726573+Activx-nV@users.noreply.github.com>
2020-11-28 02:55:57 +0200

    feat: make basic functional | script.js

--- Lesson07/script.js
+++ Lesson07/script.js
@@ -4,19 +4,21 @@ let date = new Date();
 let today = new Date().getDay() - 1;
 
 
-  
+console.log(today);
 
  for (let i = 0; i < week.length; i++) {
   if (i === today) {
-    if (week[i] === 'Saturday' || week[i] ==='Sunday') {
+    if (week[i] === 'Saturday' || week[i] === 'Sunday') {
       mainSection.innerHTML += `<strong><i>${week[i]}</i></strong></br>`;
     } else {
         mainSection.innerHTML += `<strong>${week[i]}</strong></br>`;
     }
+    } else if (today === -1 && week[i] === 'Sunday') {
+      mainSection.innerHTML += `<strong><i>${week[i]}</i></strong></br>`;
     } else if (week[i] === 'Saturday' || week[i] === 'Sunday') {
         mainSection.innerHTML += `<i>${week[i]}</i></br>`;
-    } else {
-        mainSection.innerHTML += `${week[i]}</br>`;
-   }
+    }  else {
+      mainSection.innerHTML += `${week[i]}</br>`;
+    }
  }
     
