b5ae5700a341fb65e08278c8659ee1a3d2fef1ee
Activx-nV <68726573+Activx-nV@users.noreply.github.com>
2020-12-01 21:43:49 +0200

    feat: make basic functional

--- Lesson07/script.js
+++ Lesson07/script.js
@@ -3,19 +3,18 @@ let mainSection = document.querySelector('.weekDays');
 let date = new Date();
 let today = new Date().getDay() - 1;
 
- for (let i = 0; i < week.length; i++) {
-  if (i === today) {
-    if (week[i] === 'Saturday' || week[i] === 'Sunday') {
-      mainSection.innerHTML += `<strong><i>${week[i]}</i></strong></br>`;
-    } else {
-        mainSection.innerHTML += `<strong>${week[i]}</strong></br>`;
-    }
+for (let i = 0; i < week.length; i++) {
+    if (i === today) {
+        if (week[i] === 'Saturday' || week[i] === 'Sunday') {
+            mainSection.innerHTML += `<strong><i>${week[i]}</i></strong></br>`;
+        } else {
+            mainSection.innerHTML += `<strong>${week[i]}</strong></br>`;
+        }
     } else if (today === -1 && week[i] === 'Sunday') {
-      mainSection.innerHTML += `<strong><i>${week[i]}</i></strong></br>`;
+        mainSection.innerHTML += `<strong><i>${week[i]}</i></strong></br>`;
     } else if (week[i] === 'Saturday' || week[i] === 'Sunday') {
         mainSection.innerHTML += `<i>${week[i]}</i></br>`;
-    }  else {
-      mainSection.innerHTML += `${week[i]}</br>`;
+    } else {
+        mainSection.innerHTML += `${week[i]}</br>`;
     }
- }
-    
+}
